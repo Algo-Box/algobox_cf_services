33f3bea3077d1c9cddd69be581e0bfd14f47ad4c
AbhiY98 <abhishekyadav88846@gmail.com>
2020-04-23 16:53:56 +0530

    Hotfix

--- server/Server.go
+++ server/Server.go
@@ -3,15 +3,22 @@ package server
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // Init is used to initialize a server type struct
-func (s *Server) Init(port ...string) {
-	if len(port) >= 1 {
-		s.Port = port[0]
+func (s *Server) Init() {
+	if os.Getenv("PORT") == "" {
+		s.Port = "8080"
+	} else {
+		s.Port = os.Getenv("PORT")
+		log.Println("Got port " + s.Port)
+		if s.Port == "" {
+			log.Fatal("PORT must be Set")
+		}
 	}
 	s.Router = gin.Default()
 	s.Router.Use(cors.Default())
